5a12f4bff8076345f0b0f332e038f210df29662f
Sepren <90904925+Sepren@users.noreply.github.com>
2023-03-22 00:30:25 +0000

    made the ninth task

---
+++ search_insert_position.py
@@ -0,0 +1,10 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target > nums[-1]:
+            return len(nums)
+        else:
+            for i in range(len(nums)):
+                if nums[i] == target:
+                    return i
+                if nums[i] > target:
+                    return i
